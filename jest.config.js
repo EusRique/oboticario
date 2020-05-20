ea559d94b6aaeca51510bf5b9be7e2e860972d3b
Henrique <hiquesc_@hotmail.com>
2020-05-20 15:26:53 -0300

    refactor: jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  }
-};
+  },
+}
