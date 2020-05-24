6ea621568fdb1d3d7100b6c03a6ebac3895bae4e
Henrique <hiquesc_@hotmail.com>
2020-05-24 02:15:46 -0300

    chore: change configs

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/oboticario',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/oboticario',
   port: process.env.PORT || 3000,
   jwtSecret: process.env.JWT_SECRET || 'JWG!@%$'
 }
