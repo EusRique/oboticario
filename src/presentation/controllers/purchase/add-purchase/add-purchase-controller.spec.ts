1afd58bf6b4b4d9be34c13c30a8e2d72aa9ba911
Henrique <hiquesc_@hotmail.com>
2020-05-25 13:40:37 -0300

    test: ensure AddPurchaseController returns badRequest error if validation fails

--- src/presentation/controllers/purchase/add-purchase/add-purchase-controller.spec.ts
+++ src/presentation/controllers/purchase/add-purchase/add-purchase-controller.spec.ts
@@ -1,12 +1,16 @@
 import { HttpRequest } from './add-purchase-controller-protocols'
 import { AddPurchaseController } from './add-purchase-controller'
 import { Validation } from '../../../protocols'
+import { badRequest } from '../../../helpers/http/http-helpers'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
     code: 'any_value',
     value: 'any_value',
     cpf: 'any_cpf',
+    percentage: 'any_percentage',
+    cashbackAmount: 'any_cashbackAmount',
+    status: 'any_status',
     date: 'any_date'
   }
 })
@@ -41,5 +45,12 @@ describe('AddSurvey Controller', () => {
     const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+
+  test('Should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
