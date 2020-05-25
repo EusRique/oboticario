59b6525477fee36e544d92053db3928fd957d616
Henrique <hiquesc_@hotmail.com>
2020-05-25 14:35:35 -0300

    feat: ensure AddPurchaseController return 500 if AddPurchaseUseCase throws

--- src/presentation/controllers/purchase/add-purchase/add-purchase-controller.ts
+++ src/presentation/controllers/purchase/add-purchase/add-purchase-controller.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse, Validation, AddPurchase } from './add-purchase-controller-protocols'
-import { badRequest } from '../../../helpers/http/http-helpers'
+import { badRequest, serverError } from '../../../helpers/http/http-helpers'
 
 export class AddPurchaseController implements Controller {
   constructor (
@@ -8,20 +8,24 @@ export class AddPurchaseController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { code, value, cpf, percentage, cashbackAmount, status, date } = httpRequest.body
+      await this.addPurchase.add({
+        code,
+        value,
+        cpf,
+        percentage,
+        cashbackAmount,
+        status,
+        date
+      })
+      return null
+    } catch (error) {
+      return serverError(error)
     }
-    const { code, value, cpf, percentage, cashbackAmount, status, date } = httpRequest.body
-    await this.addPurchase.add({
-      code,
-      value,
-      cpf,
-      percentage,
-      cashbackAmount,
-      status,
-      date
-    })
-    return null
   }
 }
