d7a1949a1fe31235fa19f45f60d98c5345dcffa5
Henrique <hiquesc_@hotmail.com>
2020-05-26 14:50:34 -0300

    feat: authentication rotes

--- src/main/routes/purchase-routes.ts
+++ src/main/routes/purchase-routes.ts
@@ -5,6 +5,7 @@ import { adaptMiddlewares } from '../adapters/express-middlewares-adapter'
 import { makeAuthMiddlewares } from '../factories/middlewares/auth-middlewares-factory'
 
 export default (router: Router): void => {
-  const userAuth = adaptMiddlewares(makeAuthMiddlewares('user'))
-  router.post('/purchases', userAuth, adaptRoute(makeAddPurchaseController()))
+  // Se no makeAddPurchaseController tiver setado admin a rota só será acessivel a users admin
+  const auth = adaptMiddlewares(makeAuthMiddlewares())
+  router.post('/purchases', auth, adaptRoute(makeAddPurchaseController()))
 }
