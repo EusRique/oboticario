650d67b70fc662421101d6f0ed59746c5ba8a750
Henrique <hiquesc_@hotmail.com>
2020-05-26 22:52:07 -0300

    feat: ensure LoadPurchasesController returns 200 on success

--- src/presentation/controllers/purchase/load-purchases/load-purchases-controller.ts
+++ src/presentation/controllers/purchase/load-purchases/load-purchases-controller.ts
@@ -1,9 +1,10 @@
 import { Controller, HttpRequest, HttpResponse, LoadPurchases } from './load-purchases-controller-protocols'
+import { ok } from '../../../helpers/http/http-helpers'
 
 export class LoadPurchaseController implements Controller {
   constructor (private readonly loadPurchases: LoadPurchases) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    await this.loadPurchases.load()
-    return null
+    const purchases = await this.loadPurchases.load()
+    return ok(purchases)
   }
 }
