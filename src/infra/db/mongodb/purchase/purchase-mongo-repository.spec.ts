34407cbcad54a1bcb92c86d27d2c42a53deb6c63
Henrique <hiquesc_@hotmail.com>
2020-05-27 00:33:16 -0300

    test: ensure PurchaseMongoRepository return an empty list and success

--- src/infra/db/mongodb/purchase/purchase-mongo-repository.spec.ts
+++ src/infra/db/mongodb/purchase/purchase-mongo-repository.spec.ts
@@ -3,6 +3,11 @@ import { PurchaseMongoRepository } from './purchase-mongo-repository'
 import { Collection } from 'mongodb'
 
 let purchaseCollection: Collection
+
+const makeSut = (): PurchaseMongoRepository => {
+  return new PurchaseMongoRepository()
+}
+
 describe('Purchase Mongo Repository', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -17,22 +22,53 @@ describe('Purchase Mongo Repository', () => {
     await purchaseCollection.deleteMany({})
   })
 
-  const makeSut = (): PurchaseMongoRepository => {
-    return new PurchaseMongoRepository()
-  }
-
-  test('Should add a purchase on sucess', async () => {
-    const sut = makeSut()
-    await sut.add({
-      code: 'any_code',
-      value: 0,
-      cpf: 'any_cpf',
-      percentage: 0,
-      cashbackAmount: 0,
-      status: 'any_status',
-      date: new Date()
+  describe('Add()', () => {
+    test('Should add a purchase on sucess', async () => {
+      const sut = makeSut()
+      await sut.add({
+        code: 'any_code',
+        value: 0,
+        cpf: 'any_cpf',
+        percentage: 0,
+        cashbackAmount: 0,
+        status: 'any_status',
+        date: new Date()
+      })
+      const purchase = await purchaseCollection.findOne({ code: 'any_code' })
+      expect(purchase).toBeTruthy()
     })
-    const purchase = await purchaseCollection.findOne({ code: 'any_code' })
-    expect(purchase).toBeTruthy()
+  })
+
+  describe('loadAll()', () => {
+    test('Should load all purchase on sucess', async () => {
+      await purchaseCollection.insertMany([{
+        code: 'any_code',
+        value: 0,
+        cpf: 'any_cpf',
+        percentage: 0,
+        cashbackAmount: 0,
+        status: 'any_status',
+        date: new Date()
+      }, {
+        code: 'other_code',
+        value: 0,
+        cpf: 'other_cpf',
+        percentage: 0,
+        cashbackAmount: 0,
+        status: 'other_status',
+        date: new Date()
+      }])
+      const sut = makeSut()
+      const purchase = await sut.loadAll()
+      expect(purchase.length).toBe(2)
+      expect(purchase[0].code).toBe('any_code')
+      expect(purchase[1].code).toBe('other_code')
+    })
+
+    test('Should load empty list', async () => {
+      const sut = makeSut()
+      const purchase = await sut.loadAll()
+      expect(purchase.length).toBe(0)
+    })
   })
 })
