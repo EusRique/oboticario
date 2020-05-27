604877bfb66def3d127fdb164be1755219d0f230
Henrique <hiquesc_@hotmail.com>
2020-05-27 00:32:43 -0300

    feat: ensure PurchaseMongoRepository return an empty list and success

--- src/infra/db/mongodb/purchase/purchase-mongo-repository.ts
+++ src/infra/db/mongodb/purchase/purchase-mongo-repository.ts
@@ -1,10 +1,18 @@
 import { AddPurchaseRepository } from '../../../../data/protocols/db/purchase/add-purchase-repository'
 import { AddAPurchaseModel } from '../../../../domain/usecases/add-purchase'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { LoadPurchaseRepository } from '../../../../data/protocols/db/purchase/load-purchase-repository'
+import { PurchaseModel } from '../../../../domain/models/purchase'
 
-export class PurchaseMongoRepository implements AddPurchaseRepository {
+export class PurchaseMongoRepository implements AddPurchaseRepository, LoadPurchaseRepository {
   async add (purchaseData: AddAPurchaseModel): Promise<void> {
     const purchaseCollection = await MongoHelper.getCollection('purchases')
     await purchaseCollection.insertOne(purchaseData)
+  }
+
+  async loadAll (): Promise<PurchaseModel[]> {
+    const purchaseCollection = await MongoHelper.getCollection('purchases')
+    const purchases: PurchaseModel[] = await purchaseCollection.find().toArray()
+    return purchases
   }
 }
