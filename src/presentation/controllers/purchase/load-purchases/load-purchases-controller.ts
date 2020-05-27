01a36445d970ef7c68b10d5f96ff214ad6da00a4
Henrique <hiquesc_@hotmail.com>
2020-05-26 23:17:40 -0300

    feat: ensure LoadPurchasesController returns 204 if there is no content

--- src/presentation/controllers/purchase/load-purchases/load-purchases-controller.ts
+++ src/presentation/controllers/purchase/load-purchases/load-purchases-controller.ts
@@ -1,12 +1,12 @@
 import { Controller, HttpRequest, HttpResponse, LoadPurchases } from './load-purchases-controller-protocols'
-import { ok, serverError } from '../../../helpers/http/http-helpers'
+import { ok, serverError, noContent } from '../../../helpers/http/http-helpers'
 
 export class LoadPurchaseController implements Controller {
   constructor (private readonly loadPurchases: LoadPurchases) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const purchases = await this.loadPurchases.load()
-      return ok(purchases)
+      return purchases.length ? ok(purchases) : noContent()
     } catch (error) {
       return serverError(error)
     }
